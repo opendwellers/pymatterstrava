f25c76f0177769bea0a2e5c87297327a5e000fa3
Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>
2016-06-01 16:09:03 -0400

    WIP

--- StravaBot.py
+++ StravaBot.py
@@ -1,6 +1,8 @@
 from stravalib.client import Client
+from numpy import  array_equal 
 
 import ConfigParser
+import time
 
 #client = Client()
 #access_token = client.exchange_code_for_token(client_id=11801, client_secret='f1d4b15369eb1e8f6e7bb39bbaa65942e127577d', code='139090cec37a555b4577c50a38a8114523492c23')
@@ -8,6 +10,7 @@ import ConfigParser
 #client.access_token = access_token
 #client.access_token = '1d049e3eecd42b4b3f9469922485940401031382'
 #athlete = client.get_athlete()
+#client.get_activities(
 #print("For {id}, I now have an access token ".format(id=athlete.id))
 
 #clubs = athlete.clubs
@@ -27,9 +30,35 @@ class StravaBot:
         self.clubId = Config.get('Strava', 'ClubId')
 
         self.mattermostUrl = Config.get('Mattermost', 'URL')
+        self.client = Client()
+        self.client.access_token = self.clientAccessToken
 
-        print('Bot is here :^)')
+        self.club = self.client.get_club(self.clubId)
+
+        print('Bot for {club} is here :^)'.format(club=self.clubId))
+
+    def get_club_members(self):
+        return self.client.get_club_members(self.clubId) 
 
 
-#    def run(self):
+    def run(self):
+        members = self.get_club_members()
+
+        for member in members:
+            print(member)
+
+        activities = self.client.get_club_activities(self.clubId, limit=20)
+
+        for activity in activities:
+            print(activity)
+            activity.done = True
+
+        while(1):
+            new_activities = self.client.get_club_activities(self.clubId, limit=20)
+            if (not (array_equal(new_activities, activities))):
+                print('No changes!')
+            else:
+                print('New stuff!')
+                activities = new_activities
+            time.sleep(2)
 
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from StravaBot import StravaBot
 
 if __name__ == "__main__":
     bot = StravaBot()
+    bot.run()
